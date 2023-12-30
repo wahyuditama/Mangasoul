07332922d52ffc0d71418bd0988fc5cce4d1508b
DCR <caesarwahyu18@gmail.com>
2023-12-30 11:49:39 +0700

    update file baru

--- page/menwa.php
+++ page/menwa.php
@@ -27,7 +27,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index2.html">Home</a>
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Menhua</a>
