1595c090e58162778b48f0072d8b7620be929af1
DCR <caesarwahyu18@gmail.com>
2024-01-27 22:58:46 +0700

    new update

--- page/menhua.php
+++ page/menhua.php
@@ -27,84 +27,9 @@
 
   <body style="background: url(./assets/img/pattern.jpg);">
 <!--     navbar -->
-    <nav
-      class="d-flex flex-column navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top py-3"
-      id="navbar"
-      style="transition: top 0.3s ease-in-out"
-    >
-   
-      <div class="container pb-3">
-        <div class="logos mb-2">
-          <a class="navbar-brand" href="#">COBAIN</a>
-        </div>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div
-          class="collapse navbar-collapse text-right"
-          style="width: 100%"
-          id="navbarSupportedContent"
-        >
-          <ul
-            class="navbar-nav ms-auto mb-2 mb-lg-0 justify-content-evenly"
-            style="width: 80%"
-          >
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="#" href="#layanan"
-                >Layanan</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="#" href="#portofolio"
-                >New Manga</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="#" href="#tentang"
-                >Menhwa</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="#" href="#staff"
-                >Library</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="#" href="#kontak"
-                >Kontak Kami</a
-              >
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-      <div class="header text-center fw-300 border-top">
-        <div class="content d-flex justify-content-center">
-          <h4 class="m-2 p-1"><a href="#">Menwa</a></h4>
-          <h4 class="m-2 p-1"><a href="#">Menwa</a></h4>
-          <h4 class="m-2 p-1"><a href="#">Manga</a></h4>
-          <h4 class="m-2 p-1"><a href="#">Terbaru</a></h4>
-        </div>
-      </div>
-    </nav>
+  <?php 
+    include ("../view/main view/navbar.php");
+   ?>
 
 <!--     form search -->
 <div class="container mt-5 pt-5">
